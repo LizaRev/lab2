ef77d9ef68334a9c3766a79a5a088fde3bb59148
LizaRev <9676936@stud.kai.edu.ua>
2025-09-28 15:55:23 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nA = 23; //  23 в двійковій системі  = 10111
+    int nInverta = ~ 23; // 23 після інверсії в двійковій системі у 8-бітому  для -24 = 11101000
+    cout << "nA = " <<nA << endl;
+    cout << "nInverta = "<<nInverta<<endl;
+
+    return 0;
+}
+
+
+
